30df3a78617e72d56be3c06e2e90eeaa44c3fc9c
AbdsalamHDT <abdsalam.hdtallah@gmail.com>
2023-03-16 14:55:52 -0700

    ok

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: always 0 if the program executed successfully
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
